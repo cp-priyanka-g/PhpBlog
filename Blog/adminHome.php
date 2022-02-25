cca5a1f544b404529c79028f96bced7942d9e724
PriyankaGour <priyanka.g@canopas.com>
2022-02-25 15:32:59 +0530

    Admin adding permission to user's post visibility

--- Blog/adminHome.php
+++ Blog/adminHome.php
@@ -1,3 +1,4 @@
+<?php include('server.php') ?>
 <?php 
   session_start(); 
 
@@ -14,7 +15,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Home</title>
+	<title>Admin Home Page</title>
 	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
@@ -22,6 +23,7 @@
 <div class="header">
 	<h2>Home Page</h2>
 </div>
+
 <div class="content">
   	<!-- notification message -->
   	<?php if (isset($_SESSION['success'])) : ?>
@@ -41,6 +43,46 @@
     	<p> <a href="login.php?logout='1'" style="color: red;">logout</a> </p>
     <?php endif ?>
 </div>
+
+<!-- CREATE USER POST VISIBLE-->
+	
+        <form method="post" action="adminHome.php">
+
+		<?php echo error(); ?>
+
+		<div>
+			<label>Username</label>
+			<input type="text" name="username" value="<?php echo $username; ?>">
+		</div>
+		<div>
+			<label>Email</label>
+			<input type="email" name="email" value="<?php echo $email; ?>">
+		</div>
+		<div>
+			<label>User type</label>
+			<select name="user_type" id="user_type" >
+				<option value=""></option>
+				<option value="admin">Admin</option>
+				<option value="user">User</option>
+			</select>
+		</div>
+		<div>
+        <label>Post Visible to user/label>
+			<select name="user_type" id="user_type" >
+				<option value=""></option>
+				<option value="Active">Active</option>
+				<option value="Unactive">Unactive</option>
+			</select>
+		</div>
 		
+		<div>
+			<button type="submit" class="btn" name="register_btn"> Add Permission</button>
+		</div>
+	</form>
+
+	
 </body>
 </html>
+		
+
+		
